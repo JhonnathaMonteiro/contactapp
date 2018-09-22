3e1e7f3b342b147bc729241412f307b820a60329
JhonnathaMonteiro <jhonnatha.am@gmail.com>
2018-09-22 12:13:56 -0300

    Fixed Contact Manager Navbar Link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,9 +8,9 @@ const Header = props => {
     <div>
       <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
         <div className="container">
-          <a href="/" className="navbar-brand">
+          <Link to="/" className="navbar-brand">
             {branding}
-          </a>
+          </Link>
           <div>
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
